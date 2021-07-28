97eed262deba31f37765708b05673f80448d888b
Anton.Kolmakov <anton.kolmakov@idp.zyfra.com>
2021-07-28 09:38:11 +0300

    Changes

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,6 @@ export class ServiceRegistry {
 
   private static services: Map<string, unknown> = new Map();
 
-  public static getInstance(): ServiceRegistry {
-    if (!ServiceRegistry.instance) {
-      ServiceRegistry.instance = new ServiceRegistry();
-    }
-    return ServiceRegistry.instance;
-  }
-
   public static register(key: string, service: any): void {
     if (this.services.get(key)) {
       throw new Error(`${key} - already registered`);
@@ -21,6 +14,10 @@ export class ServiceRegistry {
     this.services.set(key, service);
   }
 
+  public static resolveModel<T>(instance: Type<T>): T | undefined {
+    return (Array.from(this.services.values()) as T[]).find((v: T) => v instanceof instance);
+  }
+  
   public static unregister(key: string): void {
     this.services.delete(key);
   }
@@ -29,23 +26,26 @@ export class ServiceRegistry {
     return this.services.get(key);
   }
 
-  public static resolveModel<T>(instance: Type<T>): T | undefined {
-    return (Array.from(this.services.values()) as T[]).find((v: T) => v instanceof instance);
-  }
-
   public static reset(): void {
     this.services.clear();
   }
-}
 
-export interface IZuiApplication {
-  serviceRegistry: ServiceRegistry;
+  public static getInstance(): ServiceRegistry {
+    if (!ServiceRegistry.instance) {
+      ServiceRegistry.instance = new ServiceRegistry();
+    }
+    return ServiceRegistry.instance;
+  }
 }
 
 declare global {
   interface Window {
     ZUI_APP_CORE: IZuiApplication;
   }
+}
+
+export interface IZuiApplication {
+  serviceRegistry: ServiceRegistry;
 }
 
 window.ZUI_APP_CORE = {serviceRegistry: ServiceRegistry.getInstance()};
